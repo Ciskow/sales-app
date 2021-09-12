3bf85bf46d67a40bd9c53d0aab130366060eef54
Francisco Dionizio Salgado <franciscodsalgado@gmail.com>
2021-09-12 04:27:36 -0300

    Add navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">Sales App</h1>
+                    <p className="lead">Analyze the performance of your sales by different perspectives.</p>
+                    <hr />
+                    <p>This application consists on showing a Dashboard by using data supplied by a Back-End built with Spring Boot.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">
+                        Acess Dashboard
+                    </Link>
+                </div>
+            </div>
+        </>
     );
 }
 
