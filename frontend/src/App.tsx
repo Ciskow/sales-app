6b7887ecf587fb4a82c0087c5228576370e7fd23
Francisco Dionizio Salgado <franciscodsalgado@gmail.com>
2021-09-11 22:21:44 -0300

    Add Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <div className="container">
         <h1 className="text-primary">Hello world!</h1>
       </div>
+      <Footer />
     </>
   );
 }
